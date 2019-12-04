a1e42f8d3f70bdab7987033837948c2679582171
Poly <alexhexan22@gmail.com>
2019-12-04 22:09:19 +0100

    Turn all functions into chainable methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,48 @@
 extern crate colored;
 
 
-pub mod logger {
+pub mod storage {
     use std::fmt::Display;
     use colored::*;
 
-    // TODO: Make these chainable??
+    pub struct Logger {}
+
+    impl Logger {
+        pub fn new() -> Logger {
+            Logger {}
+        }
+
+        pub fn info<T: Display>(&self, message: T) -> &Logger {
+            println!("{} {}", "[+]".cyan(), message);
+            self
+        }
     
-    pub fn info<T: Display>(message: T) {
-        println!("{} {}", "[+]".cyan(), message);
-    }
-
-    pub fn attention<T: Display>(message: T) {
-        println!("{} {}", "[/]".magenta(), message);
-    }
-
-    pub fn warning<T: Display>(message: T) {
-        println!("{} {}", "[!]".yellow(), message);
-    }
-
-    pub fn error<T: Display>(message: T) {
-        eprintln!("{} {}", "[-]".red(), message);
-    }
-
-    pub fn panic<T, F>(message: T, handler: F) 
-        where 
-            T: Display,
-            F: FnOnce(i16)
-    {
-        let message = message.to_string();
-        eprintln!("{} {}", "[!]".red(), message.bold());
-
-        // Everything broke code
-        handler(0x0100);
+        pub fn attention<T: Display>(&self, message: T) -> &Logger {
+            println!("{} {}", "[/]".magenta(), message);
+            self
+        }
+    
+        pub fn warning<T: Display>(&self, message: T) -> &Logger {
+            println!("{} {}", "[!]".yellow(), message);
+            self
+        }
+    
+        pub fn error<T: Display>(&self, message: T) -> &Logger {
+            eprintln!("{} {}", "[-]".red(), message);
+            self
+        }
+    
+        pub fn panic<T, F>(&self, message: T, handler: F)
+            where 
+                T: Display,
+                F: FnOnce(i16)
+        {
+            let message = message.to_string();
+            eprintln!("{} {}", "[!]".red(), message.bold());
+    
+            // Everything broke code
+            handler(0x0100);
+        }
     }
 }
 
@@ -41,29 +51,43 @@ pub mod logger {
 
 #[cfg(test)]
 mod tests {
-    use super::logger;
+    use super::storage::Logger;
 
     #[test]
     fn it_works() {
-        logger::info("This is some basic info");
-        logger::info(512512);
-        logger::info(String::from("This is from a string"));
+        let logger = Logger::new();
+        let error_code = 0x0100;
 
-        logger::attention("This begs for your attention");
-        logger::attention(1337);
-        logger::attention(String::from("This begs for your attention from a string"));
+        logger
+            .info("This is some basic info")
+            .info(512512)
+            .info(String::from("This is from a string"));
 
-        logger::warning("Get warned");
-        logger::warning(5125);
-        logger::warning(String::from("Get warned from a string"));
+        logger
+            .attention("This begs for your attention")
+            .attention(1337)
+            .attention(String::from("This begs for your attention from a string"));
 
-        logger::error("This is a basic error");
-        logger::error(623462312);
-        logger::error(String::from("This is an error from a string"));
+        logger
+            .warning("Get warned")
+            .warning(5125)
+            .warning(String::from("Get warned from a string"));
 
-        logger::panic("This is all going to explode", |code| assert_eq!(code, 0x0100));
-        logger::panic(613123412, |code| assert_eq!(code, 0x0100));
-        logger::panic(String::from("This is a panic from a string"), |code| assert_eq!(code, 0x0100));
+        logger
+            .error("This is a basic error")
+            .error(623462312)
+            .error(String::from("This is an error from a string"));
+
+        logger.panic("This is all going to explode", |code| assert_eq!(code, error_code));
+        logger.panic(613123412, |code| assert_eq!(code, error_code));
+        logger.panic(String::from("This is a panic from a string"), |code| assert_eq!(code, error_code));
+
+        logger
+            .info("all")
+            .attention("together")
+            .warning("hey")
+            .error("ho")
+            .panic("Panic last because your program should die when this gets called!", |code| assert_eq!(code, error_code));
 
         // Uncomment to see the output
         // assert!(false);
