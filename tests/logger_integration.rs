59bf02f6d00e111dcf30723c9d96c54e5cf9f572
Poly <alexhexan22@gmail.com>
2019-12-06 20:13:28 +0100

    Add integration test for icons

--- tests/logger_integration.rs
+++ tests/logger_integration.rs
@@ -2,7 +2,7 @@ extern crate paris;
 
 
 use paris::Logger;
-
+use paris::LogIcon;
 
 
 
@@ -16,7 +16,8 @@ fn test_info() {
 #[test]
 fn test_error() {
     let logger = Logger::new();
-    logger.error("This is an error, but test should still work");
+    logger
+        .error("This is an error, but test should still work");
 }
 
 
@@ -31,4 +32,17 @@ fn test_warning() {
 fn test_panic() {
     let logger = Logger::new();
     logger.panic("You should die now", |c| assert_eq!(c, 0x0100));
+}
+
+
+#[test]
+fn test_icons() {
+    println!(
+        "{} All {} Of {} Them {} At {} Once", 
+        LogIcon::Info, 
+        LogIcon::Tick,
+        LogIcon::Cross,
+        LogIcon::Warning,
+        LogIcon::Heart
+    );
 }
