c67e82497b37ba6b51ac0558d79db545492eb9de
Poly <alexhexan22@gmail.com>
2020-02-27 07:55:52 +0100

    Move parsing into colors struct

--- src/colors.rs
+++ src/colors.rs
@@ -1,4 +1,5 @@
 use colored::Color;
+use regex::Regex;
 
 
 
@@ -22,6 +23,36 @@ impl Colors {
         }
 
         Colors::escape(color.to_fg_str())
+    }
+
+
+
+    pub fn parse_string<S>(input: S) -> String
+        where S: Into<String>
+    {
+        lazy_static!(
+            static ref TAG: Regex =
+                Regex::new(r"<((?:[a-zA-Z-_ ]*+)|/(?:[a-zA-Z-_ ]*+))>")
+                .unwrap();
+        );
+
+        let input = input.into();
+
+        // Nothing to escape was found
+        if TAG.find(&input).is_none() {
+            return input;
+        }
+
+        let mut output = input.clone();
+
+        for mat in TAG.captures_iter(&input) {
+            let key = &mat[0];
+            let color = &mat[1];
+
+            output = output.replace(key, &Colors::get(color));
+        }
+
+        output
     }
 
 
