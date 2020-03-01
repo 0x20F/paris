d357a5a61d9637c7df13e21212e0533f193dbb66
Poly <alexhexan22@gmail.com>
2020-03-01 10:48:13 +0100

    Build symple style enum so keys can be parsed into style numbers

---
+++ src/formatter/style.rs
@@ -0,0 +1,69 @@
+use super::ansi::ToAnsi;
+use std::str::FromStr;
+
+
+pub enum Style {
+    Bold,
+    Italic,
+    Underline,
+    Dimmed,
+    Clear
+}
+
+
+
+impl Style {
+    pub fn to_str<'a>(self) -> &'a str {
+        match self {
+            Style::Bold => "1",
+            Style::Dimmed => "2",
+            Style::Italic => "3",
+            Style::Underline => "4",
+            Style::Clear => ""
+        }
+    }
+}
+
+
+
+impl ToAnsi for Style {
+    fn from_key(key: &str) -> String {
+        let s = Style::from(key);
+
+        return Style::escape(s.to_str())
+    }
+}
+
+
+
+impl<'a> From<&'a str> for Style {
+    fn from(s: &'a str) -> Self {
+        s.parse().unwrap_or(Style::Clear)
+    }
+}
+
+
+
+impl From<String> for Style {
+    fn from(s: String) -> Self {
+        s.parse().unwrap_or(Style::Clear)
+    }
+}
+
+
+
+impl FromStr for Style {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let src = s.to_lowercase();
+
+        match src.as_ref() {
+            "bold" => Ok(Style::Bold),
+            "italic" => Ok(Style::Italic),
+            "underline" => Ok(Style::Underline),
+            "dimmed" => Ok(Style::Dimmed),
+            _ => Err(())
+        }
+    }
+}
