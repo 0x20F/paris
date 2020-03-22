4310b469a4eb121f120ae94ec3155f1d7f293d98
Poly <alexhexan22@gmail.com>
2020-03-22 13:34:26 +0100

    Move timestamp logic to different file for easier feature enable/disable

---
+++ src/timestamp/mod.rs
@@ -0,0 +1,20 @@
+extern crate chrono;
+
+use chrono::{ Timelike, Utc };
+
+
+pub fn now() -> String {
+    let now = Utc::now();
+    let (is_pm, hour) = now.hour12();
+
+    let stamp = format!(
+        "<dimmed>{:02}:{:02}:{:02}.{:03} {}: </>",
+        hour,
+        now.minute(),
+        now.second(),
+        now.nanosecond() / 1_000_000,
+        if is_pm { "PM" } else { "AM" }
+    );
+
+    stamp
+}
