0e1777c73438e5a40e51034a63db255b60de9662
Poly <alexhexan22@gmail.com>
2020-03-27 10:13:20 +0100

    Export macros

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,33 +1,34 @@
 #![macro_use]
 
 
-
+#[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
         let message = format!("<cyan><info></> {}", format!($($arg)*));
-        output::stdout(message, "\n");
+        $crate::output::stdout(message, "\n");
     }
 }
 
-
+#[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
         let message = format!("<red><cross></> {}", format!($($arg)*));
-        output::stderr(message, "\n");
+        $crate::output::stderr(message, "\n");
     }
 }
 
-
+#[macro_export]
 macro_rules! warn {
         ($($arg:tt)*) => {
         let message = format!("<yellow><warn></> {}", format!($($arg)*));
-        output::stdout(message, "\n");
+        $crate::output::stdout(message, "\n");
     }
 }
 
+#[macro_export]
 macro_rules! success {
     ($($arg:tt)*) => {
         let message = format!("<green><tick></> {}", format!($($arg)*));
-        output::stdout(message, "\n");
+        $crate::output::stdout(message, "\n");
     }
 }
