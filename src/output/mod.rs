0671860a54d970a31c3b9bcc7edd267772b19b78
Poly <alexhexan22@gmail.com>
2020-03-27 08:12:35 +0100

    Move output functions to a separate file

---
+++ src/output/mod.rs
@@ -0,0 +1,34 @@
+use crate::timestamp;
+use crate::formatter::Formatter;
+use std::fmt::Display;
+
+
+pub fn stdout<T>(message: T, line_ending: &str) where T: Display {
+    #[cfg(feature = "timestamps")] {
+        let timestamp = timestamp::now();
+        let message = format!("{}{}{}", timestamp, message, line_ending);
+        print!("{}", Formatter::colorize_string(message));
+    }
+
+    #[cfg(not(feature = "timestamps"))] {
+        let message = format!("{}{}", message, line_ending);
+        print!("{}", Formatter::colorize_string(message));
+    }
+}
+
+
+
+pub fn stderr<T>(message: T, line_ending: &str) where T: Display {
+    #[cfg(feature = "timestamps")] {
+        let timestamp = timestamp::now();
+        let message = format!("{}{}{}", timestamp, message, line_ending);
+        eprint!("{}", Formatter::colorize_string(message));
+    }
+
+    #[cfg(not(feature = "timestamps"))] {
+        let message = format!("{}{}", message, line_ending);
+        eprint!("{}", Formatter::colorize_string(message));
+    }
+}
+
+
