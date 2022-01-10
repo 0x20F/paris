4b4ca1a3bd20f3b2119f31c2760773497505daf1
0x20F <0x20fa@gmail.com>
2022-01-10 08:28:28 +0100

    fix: Only return keys that actually close themselves

--- src/formatter/keys/key_list.rs
+++ src/formatter/keys/key_list.rs
@@ -8,31 +8,65 @@ impl<'a> KeyList<'a> {
     pub fn new(input: &'a str) -> Self {
         Self { input }
     }
+
+    fn fetch_next_key(&mut self) -> Option<(Key<'a>, bool)> {
+        let mut key = None;
+
+        if let Some(i) = self.input.find('<') {
+            self.input = &self.input[i..];
+
+            let mut omit = false;
+
+            let rest = self
+                .input
+                .char_indices()
+                .take_while(|(idx, c)| {
+                    if *idx == 0 && *c == '<' {
+                        return true;
+                    }
+
+                    if *c == '<' {
+                        omit = true;
+                        return false;
+                    }
+
+                    *c != '>'
+                })
+                .last()
+                .map(|(idx, c)| idx + c.len_utf8())
+                .unwrap_or_default();
+
+            // +1 to get the last '>' that's excluded only if the key
+            // isn't a 'fake' key with a false opening
+            let adder = if omit { 0 } else { 1 };
+
+            key = Some((Key::new(&self.input[..(rest + adder)]), omit));
+            self.input = &self.input[(rest + adder)..];
+        }
+
+        key
+    }
 }
 
 impl<'a> Iterator for KeyList<'a> {
     type Item = Key<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut key = None;
+        loop {
+            let key = self.fetch_next_key();
 
-        if let Some(i) = self.input.find('<') {
-            self.input = &self.input[i..];
+            if key.is_none() { break; }
 
-            let rest = self
-                .input
-                .char_indices()
-                .take_while(|(_, c)| *c != '>')
-                .last()
-                .map(|(idx, c)| idx + c.len_utf8())
-                .unwrap_or_default();
+            let (key, omit) = key.unwrap();
 
-            // +1 to get the last '>' that's excluded
-            key = Some(Key::new(&self.input[..(rest + 1)]));
-            self.input = &self.input[(rest + 1)..];
+            // If provided key was a 'fake' with a false opening 
+            // like this "<---------------" we don't want it in the key list.
+            if omit { continue; }
+
+            return Some(key);
         }
 
-        key
+        None
     }
 }
 
@@ -46,5 +80,13 @@ mod tests {
         let key_count = KeyList::new(&input).count();
 
         assert_eq!(key_count, 3);
+    }
+
+    #[test]
+    fn ignore_fake_keys() {
+        let input = "<black><-------------------- some text <some random opening here, <and another here </>";
+        let key_count = KeyList::new(&input).count();
+
+        assert_eq!(key_count, 2);
     }
 }
